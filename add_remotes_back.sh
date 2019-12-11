e98bae6e0ef35992ffc9c71bc59c0bc2b5159e1b
Richard Skumat <gitlab@richardskumat.com>
2019-12-11 13:16:40 +0000

    Create a list of webtools for myself

---
+++ add_remotes_back.sh
@@ -0,0 +1,3 @@
+git remote add github git@github:richardskumat/webtools
+git remote add gitlab git@gitlab:richardskumat/webtools
+git remote add origin git@gitea:qwe/webtools
---
+++ readme.md
@@ -0,0 +1,7 @@
+# List of web tools
+
+Here's a list of tools I find useful.
+
+## URL encoder/decoder
+
+[URL encoder/decoder](https://meyerweb.com/eric/tools/dencoder/)
